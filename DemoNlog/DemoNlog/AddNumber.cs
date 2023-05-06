d118703b1afc418406862c5a53a937aee728d695
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-05-06 16:06:41 +0530

    [Ankita] Add : Nlog Example

---
+++ DemoNlog/DemoNlog/AddNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNlog
+{
+    public class AddNumber
+    {
+        private readonly NLog nlog = new NLog();
+        public int Sum(int first, int second)
+        {
+            if (first == 0 || second == 0)
+            {
+                nlog.LogDebug("Debug successfully");
+                nlog.LogError("Having null value");
+                nlog.LogWarning("Any number should not equal to zero");
+            }
+            int result = first + second;
+            nlog.LogDebug("Debug successfully");
+            nlog.LogInfo("Passed or executed " + result);
+            return result;
+        }
+    }
+}
---
+++ DemoNlog/DemoNlog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNlog
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            AddNumber add = new AddNumber();
+           Console.WriteLine (add.Sum(1,0));
+            Console.ReadLine();
+        }
+    }
+}
